ed0c4523d5e85adde84c10299ead0d941d8ef974
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-06-01 21:13:06 +0530

    centered the btn and resolved references issue in tsconfig.json file

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -3,7 +3,8 @@ import { Navigate, Outlet } from "react-router-dom";
 const AuthLayout = () => {
   const isAuthorized = true;
   return (
-    (isAuthorized)? (<div>
+    (isAuthorized)? (
+    <div className="flex flex-1 justify-center items-center h-screen">
       <Outlet />
     </div>):
     (<Navigate to='/' />)
