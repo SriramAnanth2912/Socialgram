d722168d51f89cc8e0ffd60f46ceb924c4bd07cd
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-06-15 17:50:35 +0530

    changed the password length to 4 invalidationfor easy testing

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,10 +4,10 @@ export const SignupValidation = z.object({
     name: z.string().min(2,{message: 'name is too short'}),
     username: z.string().min(2, {message: 'Username is too short'}),
     email: z.string().email({message: 'Invalid email'}),
-    password: z.string().min(8, {message: 'password must contain atleast 8 characters.'}),
+    password: z.string().min(4, {message: 'password must contain atleast 8 characters.'}),
   })
 
   export const SignInValidation = z.object({
     email: z.string().email({message: 'Invalid email address.'}),
-    password: z.string().min(8,{message: 'Password must contain atleast 8 characters'}),
+    password: z.string().min(4,{message: 'Password must contain atleast 8 characters'}),
   })
