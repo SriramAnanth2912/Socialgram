2312fe78d97829f7b9c12d7ea3abe476bc60ad86
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-06-27 19:37:25 +0530

    regular commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 <BrowserRouter>
     <QueryProvider>
         <AuthProvider>
             <App />
+            <Analytics />
         </AuthProvider>
     </QueryProvider>
 </BrowserRouter>);
